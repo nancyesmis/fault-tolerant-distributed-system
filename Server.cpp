ef742d0acf845375dcc9212d807011aa6ecdba50
Xiaoming Shi <xiaoming@mumble-16.cs.wisc.edu>
2012-03-13 18:17:40 -0500

    1

--- Server.cpp
+++ Server.cpp
@@ -629,8 +629,8 @@ int getServerId( )
 int main(int argc, char** argv)
 {
     init();
-    //server_id = atoi(argv[1]); //getServerId();
-    server_id = getServerId();
+    server_id = atoi(argv[1]);
+    //server_id = getServerId();
     cout << server_id << endl;
     if ( server_id < 0 )
     {
@@ -638,7 +638,7 @@ int main(int argc, char** argv)
 	exit(-1);
     }
     startThreads( waitThreads, waitUpdate);
-    if ( argc == 2 && strcmp(argv[1], "recover") == 0 )
+    if ( argc == 3 && strcmp(argv[2], "recover") == 0 )
     {
 	while ( ! recover() )
 	    sleep ( 1 );
